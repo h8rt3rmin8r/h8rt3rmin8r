e2ef96f783c43739c7b0b3f6107ed2af7829fe10
William Thompson <willinspire@users.noreply.github.com>
2018-03-04 03:58:48 -0500

    Update show-hide.js

--- js/show-hide.js
+++ js/show-hide.js
@@ -1,40 +1,40 @@
 $(document).ready(function () {
     $("#hide1").click(function () {
-        $("#line1").hide();
+        $("#sh1").hide();
     });
 });
 $(document).ready(function () {
     $("#show1").click(function () {
-        $("#line1").show();
+        $("#sh1").show();
     });
 });
 $(document).ready(function () {
     $("#hide2").click(function () {
-        $("#line2").hide();
+        $("#sh2").hide();
     });
 });
 $(document).ready(function () {
     $("#show2").click(function () {
-        $("#line2").show();
+        $("#sh2").show();
     });
 });
 $(document).ready(function () {
     $("#hide3").click(function () {
-        $("#line3").hide();
+        $("#sh3").hide();
     });
 });
 $(document).ready(function () {
     $("#show3").click(function () {
-        $("#line3").show();
+        $("#sh3").show();
     });
 });
 $(document).ready(function () {
     $("#hide4").click(function () {
-        $("#line4").hide();
+        $("#sh4").hide();
     });
 });
 $(document).ready(function () {
     $("#show4").click(function () {
-        $("#line4").show();
+        $("#sh4").show();
     });
 });
