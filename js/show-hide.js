d6b757d9c47d50074420d38d506f735a9ecb5f27
William Thompson <willinspire@users.noreply.github.com>
2018-03-04 03:44:22 -0500

    Create show-hide.js

---
+++ js/show-hide.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $("#hide1").click(function () {
+        $("#line1").hide();
+    });
+});
+$(document).ready(function () {
+    $("#show1").click(function () {
+        $("#line1").show();
+    });
+});
+$(document).ready(function () {
+    $("#hide2").click(function () {
+        $("#line2").hide();
+    });
+});
+$(document).ready(function () {
+    $("#show2").click(function () {
+        $("#line2").show();
+    });
+});
+$(document).ready(function () {
+    $("#hide3").click(function () {
+        $("#line3").hide();
+    });
+});
+$(document).ready(function () {
+    $("#show3").click(function () {
+        $("#line3").show();
+    });
+});
+$(document).ready(function () {
+    $("#hide4").click(function () {
+        $("#line4").hide();
+    });
+});
+$(document).ready(function () {
+    $("#show4").click(function () {
+        $("#line4").show();
+    });
+});
