cfc8aefd5e429454793fc1d9834717425fb0deca
William Thompson <willinspire@users.noreply.github.com>
2018-07-30 00:42:49 -0400

    Add files via upload

---
+++ cards/assets/app.version.js
@@ -0,0 +1,1 @@
+var APP_VERSION = "0.9.4"; var BUILD_NUM = "201805311527803690036";console.log(`CryptoFlashcards v.${APP_VERSION} (Build ${BUILD_NUM})`);
---
+++ cards/assets/heap.config.js
@@ -0,0 +1,2 @@
+var HEAP_ID = "3504167685";
+console.info(' => HEAP PROD CONFIG');
